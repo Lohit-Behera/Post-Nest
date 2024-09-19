53a637e71a34d286c2ade46393dc1c6695305f27
Lohit <lohitsekhar.2002@gmail.com>
2024-09-19 21:05:35 +0530

    fix access token expired auth

--- backend/src/middlewares/auth.middleware.js
+++ backend/src/middlewares/auth.middleware.js
@@ -3,9 +3,58 @@ import jwt from "jsonwebtoken";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { User } from "../models/user.model.js";
 
+function isTokenExpired(token, res) {
+    try {
+      const decoded = jwt.decode(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decoded.exp < currentTime;
+    } catch (err) {
+      res.status(401).json(new ApiResponse(401, err.message, "invalid token"));
+      return true;
+    }
+  }
+
 export const authMiddleware = asyncHandler(async (req, res, next) => {
     try {
-        const token = req.cookies.accessToken || req.header("Authorization").replace("Bearer ", "");
+        let token = req.cookies.accessToken || "";
+
+        if (!req.cookies.accessToken){
+            const refreshToken = JSON.parse(req.cookies.userInfoPostNest).refreshToken;
+
+            if (!refreshToken) {
+                res.status(401).json(new ApiResponse(401, {}, "Unauthorized"));
+            }
+            const isRefreshTokenExpired = isTokenExpired(refreshToken, res);
+
+            if (isRefreshTokenExpired) {
+                res.status(401).json(new ApiResponse(401, {}, "Refresh token expired"));
+            }
+
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+            const user = await User.findById(decoded.id);
+
+            if (!user) {
+                res.status(401).json(new ApiResponse(401, {}, "invalid token"));
+            }
+
+            const newAccessToken = user.generateAccessToken();
+
+            const newRefreshToken = user.generateRefreshToken();
+
+            res.cookie("accessToken", newAccessToken, {
+                httpOnly: true, 
+                secure: false,
+                sameSite: 'None'
+            });
+
+            res.cookie("refreshToken", newRefreshToken, {
+                httpOnly: true, 
+                secure: false,
+                sameSite: 'None'
+            });
+            token = newAccessToken;
+        }
     
         if (!token) {
             res.status(401).json(new ApiResponse(401, {}, "Unauthorized"));
@@ -15,12 +64,13 @@ export const authMiddleware = asyncHandler(async (req, res, next) => {
     
         const user = await User.findById(decoded.id);
         if (!user) {
-            res.status(401).json(new ApiResponse(401, {}, "invalid token"));
+            res.status(401).json(new ApiResponse(401, {}, "invalid token user not found"));
         }
         
         req.user = user;
         next();
     } catch (error) {
-        res.status(401).json(new ApiResponse(401, error.message, "invalid token"));
+        console.log(error);
+        res.status(401).json(new ApiResponse(401, error.message, "invalid token auth middleware"));
     }
 })

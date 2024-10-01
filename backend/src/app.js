1d0880dc735725b392c4ed862beed15f1d483b98
Lohit <lohitsekhar.2002@gmail.com>
2024-10-01 19:31:49 +0530

    fix cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,9 +3,9 @@ import cors from "cors";
 import cookieParser from "cookie-parser";
 
 const app = express();
- 
+
 app.use(cors({
-    origin: process.env.ORIGIN || "http://localhost:5173",
+    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
     credentials: true
 }));
 

a20da4d8358c2b31b875bc05472316e6af6c8197
Lohit <lohitsekhar.2002@gmail.com>
2024-10-08 21:33:37 +0530

    change redirect uri

--- backend/src/utils/googleConfig.js
+++ backend/src/utils/googleConfig.js
@@ -3,4 +3,4 @@ import { google } from "googleapis";
 const clientId = process.env.GOOGLE_CLIENT_ID;
 const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
 
-export const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, "http://localhost:5173");
+export const oAuth2Client = new google.auth.OAuth2(clientId, clientSecret, process.env.CORS_ORIGIN);
